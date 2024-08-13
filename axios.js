5b14b72569f1dee3ec6f4051cf9f30ead9bcd1ed
ASaber74 <ahmed.saber7499@gmail.github.com>
2024-08-13 22:21:58 +0300

    adding XSRFToken

--- axios.js
+++ axios.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://ecomerce-3qnz.onrender.com/api/v1/', 
-  
+  baseURL: 'https://ecomerce-3qnz.onrender.com/api/v1/',
+
   headers: {
     'Content-Type': 'application/json',
   },
   withCredentials: true,
+  withXSRFToken: true,
 });
 
 export default instance;
